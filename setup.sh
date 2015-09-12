1d72a702f10418867c398e05486a6c2fab8b0092
Arcadiy Ivanov <arcadiy@csc.com>
2015-09-12 09:07:42 -0400

    TA2824 PyEnv should install MacPorts and Yum packages.
    PyEnv should install MacPorts on OSX and Yum packages on Linux for PyEnv environment.
    Add `pyenv` alias
    Add MacPorts and yum install capabilities
    Install Postgresql 9.4.x
    Cleanup VirtualEnv activation/deactivation and make it extensible

--- setup.sh
+++ setup.sh
@@ -5,10 +5,14 @@ SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd -P)"
 
 source "$SCRIPT_DIR/setup.inc"
 
+install-prerequisites
 install-virtualenv
 install-pyhome "${1:-}"
 configure-bash-profile
 activate-venv
 configure"${2:-}"-venv
 configure-nodeenv
+configure-macports
+configure-installer
+install-binary-packages
 deactivate-venv
